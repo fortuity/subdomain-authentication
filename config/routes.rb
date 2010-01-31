5fcb189a8c5ec7b1cead248faa916c63daa6938f
Daniel Kehoe <kehoe@fortuity.com>
2010-01-31 15:39:42 -0800

    add default app layout and initial home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+	
+	map.root :controller => 'home'
+	
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
