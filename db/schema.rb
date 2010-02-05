ec72c30ba7d215063bc48e09a6590cb275ac2182
Daniel Kehoe <kehoe@fortuity.com>
2010-02-04 22:01:49 -0800

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100204234919) do
+ActiveRecord::Schema.define(:version => 20100205051942) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(:version => 20100204234919) do
   add_index "admins", ["confirmation_token"], :name => "index_admins_on_confirmation_token", :unique => true
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "subdomains", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
