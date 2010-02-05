90e3b7da949220b824994d425d6d8ed596b82c1c
Daniel Kehoe <kehoe@fortuity.com>
2010-02-04 21:59:06 -0800

    modify the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :subdomains, :dependent => :destroy
+	has_friendly_id :name
   # Include default devise modules.
   # Others available are :lockable, :timeoutable and :activatable.
   devise :authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
