ecaac02f4ffb12680e60c78230b3392e64824946
Daniel Kehoe <kehoe@fortuity.com>
2010-02-22 15:20:16 -0800

    eliminate need for a new admin to confirm from email

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules.
-  # Others available are :lockable, :timeoutable and :activatable.
-  devise :authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+  # Others available are :confirmable, :lockable, :timeoutable and :activatable.
+  devise :authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation
