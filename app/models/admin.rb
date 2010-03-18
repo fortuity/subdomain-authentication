6d39f42b693f0dab2dd57bc8188bdee902120e01
Daniel Kehoe <kehoe@fortuity.com>
2010-03-18 12:12:01 -0700

    make sure an email address and name is present and unique when a user or admin is created

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,4 +5,6 @@ class Admin < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates_presence_of :name, :email 
 end
