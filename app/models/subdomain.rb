b630faad5dcdc88ff683d048fdbc97e5bc1ba4f7
Daniel Kehoe <kehoe@fortuity.com>
2010-03-18 12:14:54 -0700

    make sure a name is present and unique when a subdomain is created

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -1,7 +1,9 @@
 class Subdomain < ActiveRecord::Base
   belongs_to :user
-	has_friendly_id :name
-	def to_param
-		name
+  has_friendly_id :name
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_presence_of :name
+  def to_param
+    name
   end
 end
